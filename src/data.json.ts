96e389e5771f4ba8eefef585873bb311b40cdd17
celina-ijs <celina.dev@instajs.com>
2023-05-12 10:03:11 +0700

    Add default data

--- src/data.json.ts
+++ src/data.json.ts
@@ -5,5 +5,20 @@ export default {
                 "address": "0x7f1EAB0db83c02263539E3bFf99b638E61916B96"
             }
         }
+    },
+    "defaultBuilderData": {
+        "description": "",
+        "logo": "ipfs://bafkreicdwbtx5niyhfzctxvxnwxjt3qfb3kyotrdpkdo26wky33lnt7lci",
+        "networks": [
+            {
+                "chainId": 43113
+            }
+        ],
+        "wallets": [
+            {
+                "name": "metamask"
+            }
+        ],
+        "defaultChainId": 43113
     }
 }
