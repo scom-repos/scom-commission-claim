4c11ed55a077f7d8aeb4b68558ed6585a722bd2b
celina-ijs <celina.dev@instajs.com>
2023-09-20 16:57:29 +0700

    Update to use FormatUtils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,37 +1,13 @@
+import { FormatUtils } from "@ijstech/components";
 import { BigNumber, ISendTxEventsOptions, Wallet } from "@ijstech/eth-wallet";
 
-export const formatNumber = (value: any, decimals?: number) => {
-  let val = value;
+export const formatNumber = (value: number | string | BigNumber, decimalFigures?: number) => {
+  if (typeof value === 'object') {
+    value = value.toString();
+  }
   const minValue = '0.0000001';
-  if (typeof value === 'string') {
-    val = new BigNumber(value).toNumber();
-  } else if (typeof value === 'object') {
-    val = value.toNumber();
-  }
-  if (val != 0 && new BigNumber(val).lt(minValue)) {
-    return `<${minValue}`;
-  }
-  return formatNumberWithSeparators(val, decimals || 4);
+  return FormatUtils.formatNumber(value, {decimalFigures: decimalFigures || 4, minValue});
 };
-
-export const formatNumberWithSeparators = (value: number, precision?: number) => {
-  if (!value) value = 0;
-  if (precision) {
-    let outputStr = '';
-    if (value >= 1) {
-      outputStr = value.toLocaleString('en-US', { maximumFractionDigits: precision });
-    }
-    else {
-      outputStr = value.toLocaleString('en-US', { maximumSignificantDigits: precision });
-    }
-
-    if (outputStr.length > 18) {
-      outputStr = outputStr.substring(0, 18) + '...';
-    }
-    return outputStr;
-  }
-  return value.toLocaleString('en-US');
-}
 
 export const registerSendTxEvents = (sendTxEventHandlers: ISendTxEventsOptions) => {
   const wallet = Wallet.getClientInstance();
